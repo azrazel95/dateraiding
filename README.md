75b789fb5f1bd13c74198b1f13c36c103389fa98
Benjamin Sprenger <115416353+azrazel95@users.noreply.github.com>
2022-12-12 18:40:36 -0500

    added readme and comments

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # dateraiding
-a calendar app
+## a calendar app
+
+The purpose of this application is a simple day-planner, with a highlighted colourcode for future, past and present tenses, all stored in local storage!
+
+## Visit the Website
+[click me!](https://azrazel95.github.io/dateraiding/)
+![the website](./assets/screenshot.png)
+
+## contributing
+
+please do not contribute
+
+## License
+
+This project has been licensed under the MIT License
+
+---
+
+© benjamin sprenger ![MIT](https://img.shields.io/github/license/azrazel95/dateraiding)
--- assets/script.js
+++ assets/script.js
@@ -1,40 +1,41 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 
+//calls the function on the callup of the page
 $(function () {
-// TODO: Add code to display the current date in the header of the page.
-  //date and time display
- 
+  //date and time display function!
   setInterval(function (){
   var now=dayjs();
    displayTimeEl = $("#currentDay");
   displayTimeEl.text(now.format("ddd-MMMM-YYYY-HH-mm-ss"));
   }, 1000);
 
-  
+  //my time constants
   const start = 00;
   const end = 24;
  
 
-  
+  //calls timeDate
 timeDate()
   function timeDate() {
+    //sets the name of the localstorage item concerning each text area
    for (i= 0; i<24; i++){
     var parentId = "hour-" + i;
     var textarea = localStorage.getItem(parentId)
+    //this is how it saves said textarea's values, #+parentId is the element we are calling, children calls that specific child, value accepts input from the textarea
     $("#" + parentId).children("textarea").val(textarea)
     if (dayjs().hour()===i){
       $("#" + parentId).children("textarea").addClass("present")
+      //this is how it sets the colours on the text areas depending on what time it is
     } else if (dayjs().hour() <i){
       $("#" + parentId).children("textarea").addClass("future")
     } else {
       $("#" + parentId).children("textarea").addClass("past")
     }
    }
-    
+    //sets the variable as the button
     timeSlotContainer = $(".saveBtn")
+    //tells the button what should happen on click
     timeSlotContainer.on("click", function(event){
+      //puts the item into local storage
       var textArea = $(this).siblings("textarea").val();
       var parentId = $(this).parent("div").attr("id")
       localStorage.setItem(parentId, textArea)
@@ -42,22 +43,6 @@ timeDate()
 
     })
   }
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
   
 });
 

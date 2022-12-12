61343a1679d704307d786568882a173242adc287
Benjamin Sprenger <115416353+azrazel95@users.noreply.github.com>
2022-12-11 21:45:28 -0500

    added js, fleshed out html

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,47 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
 $(function () {
+// TODO: Add code to display the current date in the header of the page.
+  //date and time display
+ 
+  setInterval(function (){
+  var now=dayjs();
+   displayTimeEl = $("#currentDay");
+  displayTimeEl.text(now.format("ddd-MMMM-YYYY-HH-mm-ss"));
+  }, 1000);
+
+  
+  const start = 00;
+  const end = 24;
+ 
+
+  
+timeDate()
+  function timeDate() {
+   for (i= 0; i<24; i++){
+    var parentId = "hour-" + i;
+    var textarea = localStorage.getItem(parentId)
+    $("#" + parentId).children("textarea").val(textarea)
+    if (dayjs().hour()===i){
+      $("#" + parentId).children("textarea").addClass("present")
+    } else if (dayjs().hour() <i){
+      $("#" + parentId).children("textarea").addClass("future")
+    } else {
+      $("#" + parentId).children("textarea").addClass("past")
+    }
+   }
+    
+    timeSlotContainer = $(".saveBtn")
+    timeSlotContainer.on("click", function(event){
+      var textArea = $(this).siblings("textarea").val();
+      var parentId = $(this).parent("div").attr("id")
+      localStorage.setItem(parentId, textArea)
+
+
+    })
+  }
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -18,6 +58,16 @@ $(function () {
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+  
 });
+
+
+
+
+
+
+
+
+
+
+
